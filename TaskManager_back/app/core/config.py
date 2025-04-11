a934255194bcd51261ed164ed2ddc13d4d05c2b2
slavaklevleev <slava.klevleev@gmail.com>
2025-04-11 19:27:51 +0300

    fix: docker db

--- TaskManager_back/app/core/config.py
+++ TaskManager_back/app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://myuser:mypassword@localhost/task_db"
+    DATABASE_URL: str = "postgresql+asyncpg://myuser:mypassword@db:5432/task_db"
     JWT_SECRET: str = "supersecretjwtkey"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
